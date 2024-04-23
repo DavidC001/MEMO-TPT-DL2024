d3e61c7a347b8811eea8b58e80b8bcc17c1eb9c0
Laurence Bonat <laurence.bonat@gmail.com>
2024-04-23 17:42:24 +0200

    refactor: Cleaned up memo code

--- memo/utils/adapt_helpers.py
+++ memo/utils/adapt_helpers.py
@@ -2,10 +2,6 @@ import torch
 import torch.nn as nn
 from torchvision.transforms import v2
 import torchvision.transforms as transforms
-from matplotlib import pyplot as plt
-
-
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 te_transforms = transforms.Compose([transforms.ToPILImage(),
@@ -14,7 +10,8 @@ te_transforms = transforms.Compose([transforms.ToPILImage(),
                                     transforms.ToTensor(),
                                     normalize])
 
-def adapt_single(model, image, optimizer, criterion, niter, batch_size, prior_strength):
+
+def adapt_single(model, image, optimizer, criterion, niter, batch_size, prior_strength, device):
     model.eval()
     # Using AugMix augmentation provided directly by pytorch
     augmenter = v2.AugMix()
@@ -25,7 +22,7 @@ def adapt_single(model, image, optimizer, criterion, niter, batch_size, prior_st
 
     for iteration in range(niter):
         inputs = [augmenter(image) for _ in range(batch_size)]
-        inputs = torch.stack(inputs).to(device=DEVICE)
+        inputs = torch.stack(inputs).to(device=device)
         optimizer.zero_grad()
         outputs = model(inputs)
         loss, logits = criterion(outputs)
@@ -34,7 +31,7 @@ def adapt_single(model, image, optimizer, criterion, niter, batch_size, prior_st
     nn.BatchNorm2d.prior = 1
 
 
-def test_single(model, image, label, prior_strength):
+def test_single(model, image, label, prior_strength, device):
     model.eval()
 
     if prior_strength < 0:
@@ -45,7 +42,7 @@ def test_single(model, image, label, prior_strength):
     inputs = transform(image).unsqueeze(0)
 
     with torch.no_grad():
-        outputs = model(inputs.to(device=DEVICE))
+        outputs = model(inputs.to(device=device))
         _, predicted = outputs.max(1)
         confidence = nn.functional.softmax(outputs, dim=1).squeeze()[predicted].item()
     correctness = 1 if predicted.item() == label else 0
