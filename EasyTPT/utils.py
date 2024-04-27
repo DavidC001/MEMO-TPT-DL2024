5a61a1dd459f5df5ffb714a385919035b413f3c0
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-04-27 11:21:49 +0200

    add augmix composer

--- EasyTPT/utils.py
+++ EasyTPT/utils.py
@@ -1,1 +1,48 @@
+from PIL import Image
+import numpy as np
 
+import torch
+import torchvision.transforms as transforms
+
+
+try:
+    from torchvision.transforms import InterpolationMode
+
+    BICUBIC = InterpolationMode.BICUBIC
+except ImportError:
+    BICUBIC = Image.BICUBIC
+
+import augmix_ops as augmentations
+
+
+from typing import Any
+
+
+# AugMix Transforms
+def get_preaugment():
+    return transforms.Compose(
+        [
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+        ]
+    )
+
+
+def augmix(image, preprocess, severity=1):
+    aug_list = augmentations.augmentations
+    preaugment = get_preaugment()
+    x_orig = preaugment(image)
+    x_processed = preprocess(x_orig)
+    if len(aug_list) == 0:
+        return x_processed
+    w = np.float32(np.random.dirichlet([1.0, 1.0, 1.0]))
+    m = np.float32(np.random.beta(1.0, 1.0))
+
+    mix = torch.zeros_like(x_processed)
+    for i in range(3):
+        x_aug = x_orig.copy()
+        for _ in range(np.random.randint(1, 4)):
+            x_aug = np.random.choice(aug_list)(x_aug, severity)
+        mix += w[i] * preprocess(x_aug)
+    mix = m * x_processed + (1 - m) * mix
+    return mix
