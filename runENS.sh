59481fa56a717b0a191d7b4bc970547507ca5af0
DavidC001 <davidecavicchini@hotmail.com>
2024-04-30 23:21:14 +0200

    feat: sh files to run on cluster

---
+++ runENS.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH -p edu-20h
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=1024M
+#SBATCH -N 1
+#SBATCH -t 0-20:00
+
+module load cuda/12.1
+
+source /home/davide.cavicchini/.bashrc
+conda activate SIV_hpe
+
+python3 Ensemble/main.py
