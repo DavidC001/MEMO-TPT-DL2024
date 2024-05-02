23f1bbf2225857cb2b4d4b96f94d023b15b8886b
Laurence Bonat <laurence.bonat@gmail.com>
2024-05-02 13:15:18 +0200

    fix: changed how device is passed to the model functions

--- memo/test_calls/test_adapt.py
+++ memo/test_calls/test_adapt.py
@@ -14,9 +14,6 @@ from memo.utils.train_helpers import memo_build_model
 from dataloaders.dataloader import get_dataloaders
 from memo.utils.adapt_helpers import memo_transforms
 
-# TODO: change to passed device
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-
 
 def memo_marginal_entropy(outputs):
     logits = outputs - outputs.logsumexp(dim=-1, keepdim=True)
@@ -26,8 +23,8 @@ def memo_marginal_entropy(outputs):
     return -(avg_logits * torch.exp(avg_logits)).sum(dim=-1), avg_logits
 
 
-def memo_test_adapt(model_name, batch_size, lr, weight_decay, opt, niter, prior_strength):
-    net = memo_build_model(model_name=model_name, device=DEVICE, prior_strength=prior_strength)
+def memo_test_adapt(model_name, batch_size, lr, weight_decay, opt, niter, prior_strength, device):
+    net = memo_build_model(model_name=model_name, device=device, prior_strength=prior_strength)
 
     imageNet_A, imageNet_V2 = get_dataloaders('datasets', memo_transforms)
 
@@ -42,8 +39,8 @@ def memo_test_adapt(model_name, batch_size, lr, weight_decay, opt, niter, prior_
         data = imageNet_A[i]
         image = data["img"]
         label = int(data["label"])
-        memo_adapt_single(net2, image, optimizer, memo_marginal_entropy, niter, batch_size, prior_strength, DEVICE)
-        correct.append(memo_test_single(net2, image, label, prior_strength, DEVICE)[0])
+        memo_adapt_single(net2, image, optimizer, memo_marginal_entropy, niter, batch_size, prior_strength, device)
+        correct.append(memo_test_single(net2, image, label, prior_strength, device)[0])
         if (i % 100 == 0): print(f'\nMEMO adapt test error A {(1 - np.mean(correct)) * 100:.2f}')
 
     print(f'MEMO adapt test error A {(1 - np.mean(correct)) * 100:.2f}')
@@ -54,14 +51,14 @@ def memo_test_adapt(model_name, batch_size, lr, weight_decay, opt, niter, prior_
         data = imageNet_V2[i]
         image = data["img"]
         label = int(data["label"])
-        memo_adapt_single(net2, image, optimizer, memo_marginal_entropy, niter, batch_size, prior_strength, DEVICE)
-        correct.append(memo_test_single(net2, image, label, prior_strength, DEVICE)[0])
+        memo_adapt_single(net2, image, optimizer, memo_marginal_entropy, niter, batch_size, prior_strength, device)
+        correct.append(memo_test_single(net2, image, label, prior_strength, device)[0])
 
     print(f'MEMO adapt test error V2 {(1 - np.mean(correct)) * 100:.2f}')
 
 
-def memo_optimizer_model(model_name, lr, weight_decay, opt, prior_strength):
-    net = memo_build_model(model_name=model_name, device=DEVICE, prior_strength=prior_strength)
+def memo_optimizer_model(model_name, lr, weight_decay, opt, prior_strength, device):
+    net = memo_build_model(model_name=model_name, device=device, prior_strength=prior_strength)
     optimizer = optim.SGD(net.parameters(), lr=lr, weight_decay=weight_decay)
     if opt == 'adamw':
         optimizer = optim.AdamW(net.parameters(), lr=lr, weight_decay=weight_decay)
