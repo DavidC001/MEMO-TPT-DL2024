f8201142c438311c8534c2b6808f67147b68e8a6
DavidC001 <davidecavicchini@hotmail.com>
2024-05-13 06:35:09 +0200

    feat: imagenet A and V2 scripts

--- Ensemble/ImageNetV2.py
+++ Ensemble/ImageNetV2.py
@@ -15,20 +15,20 @@ from EasyTPT.utils import get_datasets
 from EasyTPT.models import EasyTPT
 from EasyTPT.main import avg_entropy
 
-def TPT_get_model(device, base_prompt="A bad photo of a [CLS].", arch="RN50", splt_ctx= True):
+def TPT_get_model(device, classnames=imagenet_classes,base_prompt="A bad photo of a [CLS].", arch="RN50", splt_ctx= True):
+    
     model = EasyTPT(
         base_prompt=base_prompt,
         arch=arch,
         splt_ctx=splt_ctx,
-        classnames=imagenet_classes,
+        classnames=classnames,
         device=device
     )
         
 
     return model
-def TPT(device, naug=64):
+def TPT(device, naug=30):
     # prepare TPT
-    tpt = TPT_get_model(device=device)
 
     if not torch.cuda.is_available():
         print("Using CPU this is no bueno")
@@ -36,12 +36,16 @@ def TPT(device, naug=64):
         print("Using GPU, brace yourself!")
 
     datasetRoot = "datasets"
-    imageNetA, _, _, _, imageNetV2, _, _, _ = get_datasets(datasetRoot, naug)
+    _, _, _, _, imageNetV2, _, imageNetV2CustomNames, imageNetV2Map = get_datasets(datasetRoot, naug, all_classes=False)
+    tpt = TPT_get_model(device=device, classnames=imageNetV2CustomNames)
+    
+    return tpt, imageNetV2, imageNetV2Map
 
-    return tpt, imageNetA, imageNetV2
+def TPT_inference(tpt:EasyTPT, images, device="cuda", temp=1):
+    #empty memory
+    torch.cuda.empty_cache()
 
-def TPT_inference(tpt:EasyTPT, images, device, temp=1):
-    outputs = tpt(images)
+    outputs = tpt(images, top=0.2)
 
     outputs = outputs / temp
     outputs = torch.nn.functional.log_softmax(outputs, dim=1)
@@ -55,22 +59,34 @@ def memo(device):
     # prepare MEMO
     memo = memo_build_model(model_name="RN50", device=device, prior_strength=0.94)
 
-    imageNet_A, imageNet_V2 = get_dataloaders('datasets', memo_transforms)
+    _, imageNet_V2 = get_dataloaders('datasets', memo_transforms)
 
-    return memo, imageNet_A, imageNet_V2
+    return memo, imageNet_V2
 
-def memo_inference(memo, image, device, naug=8, temp=1):
+def memo_inference(memo, image, device, classes, naug=30, temp=1):
     augmenter = AugMix()
 
-    inputs = [image] + [augmenter(image) for _ in range(naug)]
+    #generate tensor with the image and its augmentations
+    inputs = [image] + [augmenter(image) for _ in range(naug-1)]
     inputs = torch.stack(inputs).to(device=device)
 
     outputs = memo(inputs)
+    mapped_outputs = torch.zeros(outputs.shape[0], len(classes))
+    for i,out in enumerate(outputs):
+        mapped_outputs[i] = out[classes]
 
-    outputs = outputs / temp
-    outputs = torch.nn.functional.log_softmax(outputs, dim=1)
+    #remove outputs not in the 200 classes used in ImageNet-A
+    batch_entropy = -(outputs.softmax(1) * outputs.log_softmax(1)).sum(1)
+    idx = torch.argsort(batch_entropy, descending=False)[
+            : int(batch_entropy.size()[0] * 0.2)
+    ]
+    outputs = outputs[idx]
+    
 
-    return outputs
+    mapped_outputs = mapped_outputs / temp
+    mapped_outputs = torch.nn.functional.log_softmax(mapped_outputs, dim=1)
+
+    return mapped_outputs
 
 def marginal_entropy(outputs):
     logits = outputs - outputs.logsumexp(dim=-1, keepdim=True)
@@ -103,7 +119,7 @@ def loss(TPT_outs, memo_outs):
         TPT_ent = entropy(avg_TPT_outs)
         scale = TPT_ent / (TPT_ent + memo_ent)
     
-    #print(f"\t\tscale TPT: {1-scale} - TPT entropy: {TPT_ent}\n\t\tscale MEMO: {scale} - MEMO entropy: {memo_ent}")
+    print(f"\t\tscale TPT: {1-scale} - TPT entropy: {TPT_ent}\n\t\tscale MEMO: {scale} - MEMO entropy: {memo_ent}")
 
     # calculate average logits
     avg_logits = (1-scale) * avg_TPT_outs + scale * avg_memo_outs
@@ -141,7 +157,7 @@ def printOutput(output, top=5, pre=""):
         for idx in indices:
             print(f"{pre}\tcLass {idx.item()}: {classnames[idx.item()]}, confidence: {output[i][idx].item()}")
 
-def test(tpt_model:EasyTPT, memo_model, tpt_data, memo_data, device, niter=1):
+def test(tpt_model:EasyTPT, memo_model, tpt_data, mapping, memo_data, classesID, device="cuda", niter=1):
     classnames = get_classes_names()
     correct = 0
     correctTPT = 0
@@ -150,8 +166,11 @@ def test(tpt_model:EasyTPT, memo_model, tpt_data, memo_data, device, niter=1):
 
     initOptimMEMO = optim.AdamW(memo_model.parameters(), lr= 0.00025, weight_decay=0)
 
-    TPT_temp = 1
-    MEMO_temp = 1
+    TPT_temp = 1.55
+    MEMO_temp = 0.7
+
+    testTPT = True
+    testMEMO = True
 
     #shuffle the data
     indx = np.random.permutation(range(len(tpt_data)))
@@ -173,14 +192,17 @@ def test(tpt_model:EasyTPT, memo_model, tpt_data, memo_data, device, niter=1):
 
         print (f"Testing on {i} - name: {name} - label: {label}")
 
-        output_base_TPT, _ = tpt_model.predict(img_TPT, ttt_steps=niter)
-        if output_base_TPT == label: 
-            correctTPT += 1
-        print(f"TPT accuracy: {correctTPT/cnt} - tested: {cnt} / {len(tpt_data)}")
+        if (testTPT):
+            output_base_TPT, _ = tpt_model.predict(img_TPT, ttt_steps=niter)
+            output_base_TPT = mapping[output_base_TPT]
+            if output_base_TPT == label: 
+                correctTPT += 1
+            print(f"TPT accuracy: {correctTPT/cnt} - predicted: {classnames[output_base_TPT]} - label: {name} - tested: {cnt} / {len(tpt_data)}")
 
-        memo_adapt_single(memo, img_MEMO, optimizerMEMO, marginal_entropy, niter, 8, device)
-        correctMEMO += memo_test_single(memo, img_MEMO, label, device)[0]
-        print(f"MEMO accuracy: {correctMEMO/cnt} - tested: {cnt} / {len(tpt_data)}")
+        if (testMEMO):
+            memo_adapt_single(memo, img_MEMO, optimizerMEMO, marginal_entropy, niter, 8, device)
+            correctMEMO += memo_test_single(memo, img_MEMO, label, device)[0]
+            print(f"MEMO accuracy: {correctMEMO/cnt} - tested: {cnt} / {len(tpt_data)}")
 
         tpt_model.reset()
         memo = deepcopy(memo_model)
@@ -192,7 +214,7 @@ def test(tpt_model:EasyTPT, memo_model, tpt_data, memo_data, device, niter=1):
             TPT_outs = TPT_inference(tpt_model, img_TPT, "cuda", temp=TPT_temp)
             #print(f"\t\tTPT output:")
             #printOutput(torch.exp(TPT_outs), pre="\t\t\t")
-            MEMO_outs = memo_inference(memo, img_MEMO, "cuda", temp=MEMO_temp)
+            MEMO_outs = memo_inference(memo, img_MEMO, classes=classesID, device="cuda", temp=MEMO_temp)
             #print(f"\t\tMEMO output:")
             #printOutput(torch.exp(MEMO_outs), pre="\t\t\t")
 
@@ -214,13 +236,15 @@ def test(tpt_model:EasyTPT, memo_model, tpt_data, memo_data, device, niter=1):
             #print(f"TPT output:")
             #printOutput(TPT_prob, pre="\t")
 
-            MEMO_out = torch.log_softmax(memo(img_MEMO.unsqueeze(0).to(device)) / MEMO_temp, dim=1)
-            MEMO_ent = entropy(MEMO_out[0])
+            MEMO_outs = torch.log_softmax(memo(img_MEMO.unsqueeze(0).to(device)) / MEMO_temp, dim=1)
+            for MEMO_out in MEMO_outs:
+                MEMO_out = MEMO_out[classesID]
+            MEMO_ent = entropy(MEMO_outs[0])
             #MEMO_prob = torch.exp(MEMO_out)
             #print(f"MEMO output:")
             #printOutput(MEMO_prob, pre="\t")
 
-            TPT_out = TPT_out.to(MEMO_out.device) #bring the outputs to the same device
+            TPT_out = TPT_out.to(MEMO_outs.device) #bring the outputs to the same device
 
             scale = TPT_ent / (TPT_ent + MEMO_ent)
 
@@ -234,12 +258,13 @@ def test(tpt_model:EasyTPT, memo_model, tpt_data, memo_data, device, niter=1):
             #breakpoint()
             #get max as prediction
             _, predicted = out.max(1)
+            predicted = mapping[predicted.item()]
 
-            if predicted.item() == label:
+            if predicted == label:
                 correct += 1
             
 
-            print(f"Ensemble accuracy: {correct/cnt} - predicted: {classnames[predicted.item()]} - label: {name} - tested: {cnt} / {len(tpt_data)}")
+            print(f"Ensemble accuracy: {correct/cnt} - predicted: {classnames[predicted]} - label: {name} - tested: {cnt} / {len(tpt_data)}")
 
 def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -248,13 +273,16 @@ def main():
     torch.manual_seed(0)
     np.random.seed(0)
 
-    tpt_model, tpt_dataA, tpt_dataV2 = TPT("cuda")
-    memo_model, memo_dataA, memo_dataV2 = memo("cuda")
-    
+    tpt_model, tpt_dataA, mapping = TPT("cuda")
+    for i,id in enumerate(mapping):
+        mapping[i] = int(id)
+    classesID = []
+    for classID in mapping:
+        classesID.append(classID)
+    memo_model, memo_dataA = memo("cuda")
+
     print("Testing on ImageNet-A")
-    test(tpt_model, memo_model, tpt_dataA, memo_dataA, device)
-    print("\nTesting on ImageNet-V2")
-    test(tpt_model, memo_model, tpt_dataV2, memo_dataV2, device)
+    test(tpt_model, memo_model, tpt_dataA, mapping, memo_dataA, classesID, device)
 
 if __name__ == "__main__":
     main()
