1e15d26e3f0b3825843c8176c72990d1d70e2983
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-06-01 19:46:27 +0200

    remove eval clip from cli args

--- EasyTPT/main.py
+++ EasyTPT/main.py
@@ -21,19 +21,6 @@ from EasyTPT.tpt_classnames.imagenet_variants import imagenet_a_mask
 torch.autograd.set_detect_anomaly(True)
 
 
-def tpt_avg_entropy(outputs):
-
-    logits = outputs - outputs.logsumexp(
-        dim=-1, keepdim=True
-    )  # logits = outputs.log_softmax(dim=1) [N, 1000]
-    avg_logits = logits.logsumexp(dim=0) - np.log(
-        logits.shape[0]
-    )  # avg_logits = logits.mean(0) [1, 1000]
-    min_real = torch.finfo(avg_logits.dtype).min
-    avg_logits = torch.clamp(avg_logits, min=min_real)
-    return -(avg_logits * torch.exp(avg_logits)).sum(dim=-1)
-
-
 def main():
     args = get_args()
     pprint(args)
@@ -46,7 +33,6 @@ def main():
     AUGS = args["augs"]
     TTT_STEPS = args["tts"]
     AUGMIX = args["augmix"]
-    EVAL_CLIP = args["clip"]
     ALIGN_STEPS = args["align_steps"]
     ENSEMBLE = args["ensemble"]
 
@@ -93,7 +79,7 @@ def main():
     random.seed(SEED)
     np.random.seed(SEED)
     np.random.shuffle(idxs)
-    # for i, data in enumerate(imageNet_A):
+
     for i in idxs:
         data = imageNet_A[i]
         label = data["label"]
@@ -117,7 +103,6 @@ def main():
         print(f"TPT Accuracy: {round(tpt_acc,3)}")
         print(f"GT: \t{name}\nTPT: \t{tpt_predicted}")
         print(f"after {cnt} samples\n")
-    # breakpoint()
 
 
 if __name__ == "__main__":
