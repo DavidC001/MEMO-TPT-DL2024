26a519c424b5554134f2b6f2927dd98c4d847088
Laurence Bonat <laurence.bonat@gmail.com>
2024-06-02 12:22:06 +0200

    feat(MEMO): better tests and possibility to choose if running v2,A

--- memo/main.py
+++ memo/main.py
@@ -13,12 +13,34 @@ from memo.utils import memo_get_datasets
 from memo.models import EasyMemo
 
 
-def MEMO_testing_step(model, dataset, mapping: bool | list, test):
+def MEMO_testing_step(test, arguments):
     print(f"Starting {test} evaluation...")
-    start = time.time()
+    device = arguments["memo"]["device"]
+    mapping = arguments["memo"]["mapping"]
+    prior_strength = arguments["memo"]["prior_strength"]
+    lr = arguments["memo"]["lr"]
+    weight_decay = arguments["memo"]["weight_decay"]
+    opt = arguments["memo"]["opt"]
+    niter = arguments["memo"]["niter"]
+    top = arguments["memo"]["top"]
+    ensemble = arguments["memo"]["ensemble"]
+    dataset_root = arguments["dataset"]["dataset_root"]
+    naugs = arguments["dataset"]["naug"]
+    aug_type = arguments["dataset"]["aug_type"]
+    weights = True if "weights" in arguments.keys() else False
 
     np.random.seed(0)
     torch.manual_seed(0)
+
+    weights = models.ResNet50_Weights.DEFAULT if weights else models.ResNet50_Weights.IMAGENET1K_V1
+    net = models.resnet50(weights=weights).to(device)
+    if "drop" in arguments.keys():
+        net.layer4.add_module('dropout', nn.Dropout(arguments["drop"], inplace=True))
+
+    model = EasyMemo(net, device, mapping, prior_strength=prior_strength, top=top, ensemble=ensemble, lr=lr,
+                     weight_decay=weight_decay, opt=opt, niter=niter)
+    imageNet_A, imageNet_V2 = memo_get_datasets(aug_type, naugs, dataset_root)
+    dataset = imageNet_A if arguments['dataset']['imageNetA'] else imageNet_V2
 
     correct = 0
     cnt = 0
@@ -57,124 +79,265 @@ if __name__ == "__main__":
         default='datasets',
         metavar="",
     )
+    parser.add_argument(
+        "--data-to-test",
+        type=str,
+        help="Which dataset to test between 'a', 'v2', 'both'",
+        default="both",
+        metavar="",
+    )
+
     args = vars(parser.parse_args())
+    DATASET_ROOT = args["datasets_root"]
+    DATASET_TO_TEST = args["data_to_test"]
 
     imageNet_A, imageNet_V2 = memo_get_datasets('augmix', 1, args["datasets_root"])
     mapping_a = [int(x) for x in imageNet_A.classnames.keys()]
     mapping_v2 = [int(x) for x in imageNet_V2.classnames.keys()]
-    net = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-    memo = []
+
+    del imageNet_A, imageNet_V2
 
     memo_tests = False
     drop_tests = True
     ensemble_tests = True
     baseline_tests = False
 
-    # Baseline tests
-    if baseline_tests:
-        memo = EasyMemo(net.to(device), device, mapping_a, prior_strength=1, top=1, ensemble=True)
-        test_name = "Baseline ImageNetA"
-        MEMO_testing_step(memo, imageNet_A, mapping_a, test_name)
+    base_test = {
+        "memo": {
+            "device": device,
+            "prior_strength": 1.0,
+            "lr": 0.005,
+            "weight_decay": 0.0001,
+            "opt": 'sgd',
+            "niter": 1,
+            "top": 1,
+            "ensemble": False,
+        },
+        "dataset": {
+            "imageNetA": True,
+            "naug": 1,
+            "dataset_root": DATASET_ROOT,
+            "aug_type": "augmix",
+        },
+    }
 
-        test_name = "Baseline ImageNetV2"
-        del memo
-        memo = EasyMemo(net.to(device), device, mapping_v2, prior_strength=1, top=1, ensemble=True)
-        MEMO_testing_step(memo, imageNet_V2, mapping_v2, test_name)
+    tests = {
+        "Baseline ImageNetA": {
+            "memo": {
+                "mapping": mapping_a,
+                "ensemble": True,
+                "top": 1,
+            },
+            "dataset": {
+                "imageNetA": True,
+            },
+            "run": baseline_tests and (DATASET_TO_TEST in ["a", "both"]),
+        },
+        "Baseline ImageNetV2": {
+            "memo": {
+                "mapping": mapping_v2,
+                "ensemble": True,
+                "top": 1,
+            },
+            "dataset": {
+                "imageNetA": False,
+            },
+            "run": baseline_tests and (DATASET_TO_TEST in ["v2", "both"]),
+        },
+        "Baseline ImageNetA ResNet50 weights V1": {
+            "memo": {
+                "mapping": mapping_a,
+                "ensemble": True,
+                "top": 1,
+            },
+            "dataset": {
+                "imageNetA": True,
+            },
+            "weights": "v1",
+            "run": baseline_tests and (DATASET_TO_TEST in ["a", "both"]),
+        },
+        "Baseline ImageNetV2 ResNet50 weights V1": {
+            "memo": {
+                "mapping": mapping_v2,
+                "ensemble": True,
+                "top": 1,
+            },
+            "dataset": {
+                "imageNetA": False,
+            },
+            "weights": "v1",
+            "run": baseline_tests and (DATASET_TO_TEST in ["v2", "both"]),
+        },
+        "MEMO ImageNetA, without topk selection": {
+            "memo": {
+                "top": 1,
+                "mapping": mapping_a,
+                "prior_strength": 0.94
+            },
+            "dataset": {
+                "imageNetA": True,
+                "naug": 8,
+                "aug_type": "cut",
+            },
+            "run": memo_tests and (DATASET_TO_TEST in ["a", "both"]),
+        },
+        "MEMO ImageNetV2, without topk selection": {
+            "memo": {
+                "top": 1,
+                "mapping": mapping_v2,
+                "prior_strength": 0.94
+            },
+            "dataset": {
+                "imageNetA": False,
+                "naug": 8,
+                "aug_type": "cut",
+            },
+            "run": memo_tests and (DATASET_TO_TEST in ["v2", "both"]),
+        },
+        "MEMO ImageNetA, with topk selection": {
+            "memo": {
+                "top": 0.1,
+                "mapping": mapping_a,
+                "prior_strength": 0.94
+            },
+            "dataset": {
+                "imageNetA": True,
+                "naug": 64,
+                "aug_type": "cut",
+            },
+            "run": memo_tests and (DATASET_TO_TEST in ["a", "both"]),
+        },
+        "MEMO ImageNetV2, with topk selection": {
+            "memo": {
+                "top": 0.1,
+                "mapping": mapping_v2,
+                "prior_strength": 0.94
+            },
+            "dataset": {
+                "imageNetA": False,
+                "naug": 64,
+                "aug_type": "cut",
+            },
+            "run": memo_tests and (DATASET_TO_TEST in ["v2", "both"]),
+        },
+        "DROP ImageNetA, without topk selection": {
+            "memo": {
+                "top": 1,
+                "mapping": mapping_a,
+                "ensemble": True,
+            },
+            "dataset": {
+                "imageNetA": True,
+                "naug": 8,
+                "aug_type": "identity",
+            },
+            "drop": 0.5,
+            "run": drop_tests and (DATASET_TO_TEST in ["a", "both"]),
+        },
+        "DROP ImageNetV2, without topk selection": {
+            "memo": {
+                "top": 1,
+                "mapping": mapping_v2,
+                "ensemble": True,
+            },
+            "dataset": {
+                "imageNetA": False,
+                "naug": 8,
+                "aug_type": "identity",
+            },
+            "drop": 0.5,
+            "run": drop_tests and (DATASET_TO_TEST in ["v2", "both"]),
+        },
+        "DROP ImageNetA, with topk selection": {
+            "memo": {
+                "top": 0.1,
+                "mapping": mapping_a,
+                "ensemble": True,
+            },
+            "dataset": {
+                "imageNetA": True,
+                "naug": 64,
+                "aug_type": "identity",
+            },
+            "drop": 0.5,
+            "run": drop_tests and (DATASET_TO_TEST in ["a", "both"]),
+        },
+        "DROP ImageNetV2, with topk selection": {
+            "memo": {
+                "top": 0.1,
+                "mapping": mapping_v2,
+                "ensemble": True,
+            },
+            "dataset": {
+                "imageNetA": False,
+                "naug": 64,
+                "aug_type": "identity",
+            },
+            "drop": 0.5,
+            "run": drop_tests and (DATASET_TO_TEST in ["v2", "both"]),
+        },
+        "DROP ImageNetA, cut ensemble without topk selection": {
+            "memo": {
+                "top": 1,
+                "mapping": mapping_a,
+                "ensemble": True,
+            },
+            "dataset": {
+                "imageNetA": True,
+                "naug": 8,
+                "aug_type": "cut",
+            },
+            "drop": 0,
+            "run": ensemble_tests and (DATASET_TO_TEST in ["a", "both"]),
+        },
+        "DROP ImageNetV2, cut ensemble  without topk selection": {
+            "memo": {
+                "top": 1,
+                "mapping": mapping_v2,
+                "ensemble": True,
+            },
+            "dataset": {
+                "imageNetA": False,
+                "naug": 8,
+                "aug_type": "identity",
+            },
+            "drop": 0,
+            "run": ensemble_tests and (DATASET_TO_TEST in ["v2", "both"]),
+        },
+        "DROP ImageNetA, cut ensemble  with topk selection": {
+            "memo": {
+                "top": 0.1,
+                "mapping": mapping_a,
+                "ensemble": True,
+            },
+            "dataset": {
+                "imageNetA": True,
+                "naug": 64,
+                "aug_type": "identity",
+            },
+            "drop": 0,
+            "run": ensemble_tests and (DATASET_TO_TEST in ["a", "both"]),
+        },
+        "DROP ImageNetV2, cut ensemble  with topk selection": {
+            "memo": {
+                "top": 0.1,
+                "mapping": mapping_v2,
+                "ensemble": True,
+            },
+            "dataset": {
+                "imageNetA": False,
+                "naug": 64,
+                "aug_type": "identity",
+            },
+            "drop": 0,
+            "run": ensemble_tests and (DATASET_TO_TEST in ["v2", "both"]),
+        },
+    }
 
-        test_name = "ResNet50 V1 weights ImageNetA"
-        del memo
-        net = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
-        memo = EasyMemo(net.to(device), device, mapping_a, prior_strength=1, top=1, ensemble=True)
-        MEMO_testing_step(memo, imageNet_A, mapping_a, test_name)
-
-        test_name = "ResNet50 V1 weights ImageNetV2"
-        del memo
-        memo = EasyMemo(net.to(device), device, mapping_v2, prior_strength=1, top=1, ensemble=True)
-        MEMO_testing_step(memo, imageNet_V2, mapping_v2, test_name)
-    del net, imageNet_A, imageNet_V2
-    net = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-    imageNet_A, imageNet_V2 = memo_get_datasets('cut', 8)
-    if memo_tests:
-        test_name = "MEMO ImageNetA, without topk selection"
-        memo = EasyMemo(net.to(device), device, mapping_a, prior_strength=0.94, top=1)
-        MEMO_testing_step(memo, imageNet_A, mapping_a, test_name)
-
-        test_name = "MEMO ImageNetV2, without topk selection"
-        del memo
-        memo = EasyMemo(net.to(device), device, mapping_v2, prior_strength=0.94, top=1)
-        MEMO_testing_step(memo, imageNet_V2, mapping_v2, test_name)
-
-        test_name = "MEMO ImageNetA, with topk selection"
-        del memo, imageNet_V2, imageNet_A
-        imageNet_A, imageNet_V2 = memo_get_datasets('cut', 64)
-        memo = EasyMemo(net.to(device), device, mapping_a, prior_strength=0.94, top=0.1)
-        MEMO_testing_step(memo, imageNet_A, mapping_a, test_name)
-
-        test_name = "MEMO ImageNetV2, with topk selection"
-        del memo
-        memo = EasyMemo(net.to(device), device, mapping_v2, prior_strength=0.94, top=0.1)
-        MEMO_testing_step(memo, imageNet_V2, mapping_v2, test_name)
-
-    del net
-    net = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-    # Dropout tests
-    if drop_tests:
-        test_name = "DROP ImageNetA, without topk selection"
-        net.layer4.add_module('dropout', nn.Dropout(0.5, inplace=True))
-        del memo, imageNet_V2, imageNet_A
-        imageNet_A, imageNet_V2 = memo_get_datasets('identity', 8)
-        memo = EasyMemo(net.to(device), device, mapping_a, prior_strength=1, top=1, ensemble=True)
-        MEMO_testing_step(memo, imageNet_A, mapping_a, test_name)
-
-        test_name = "DROP ImageNetV2, without topk selection"
-        del memo, net
-        net = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-        net.layer4.add_module('dropout', nn.Dropout(0.5, inplace=True))
-        memo = EasyMemo(net.to(device), device, mapping_v2, prior_strength=1, top=1, ensemble=True)
-        MEMO_testing_step(memo, imageNet_V2, mapping_v2, test_name)
-
-        test_name = "DROP ImageNetA, with topk selection"
-        del memo, imageNet_V2, imageNet_A, net
-        net = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-        net.layer4.add_module('dropout', nn.Dropout(0.5, inplace=True))
-        imageNet_A, imageNet_V2 = memo_get_datasets('identity', 64)
-        memo = EasyMemo(net.to(device), device, mapping_a, prior_strength=1, top=0.1, ensemble=True)
-        MEMO_testing_step(memo, imageNet_A, mapping_a, test_name)
-
-        test_name = "DROP ImageNetV2, with topk selection"
-        del memo, net
-        net = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-        net.layer4.add_module('dropout', nn.Dropout(0.5, inplace=True))
-        memo = EasyMemo(net.to(device), device, mapping_v2, prior_strength=1, top=0.1, ensemble=True)
-        MEMO_testing_step(memo, imageNet_V2, mapping_v2, test_name)
-
-    if ensemble_tests:
-        test_name = "DROP ImageNetA, cut ensemble without topk selection"
-        del memo, imageNet_V2, imageNet_A, net
-        net = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-        net.layer4.add_module('dropout', nn.Dropout(0, inplace=True))
-        imageNet_A, imageNet_V2 = memo_get_datasets('cut', 8)
-        memo = EasyMemo(net.to(device), device, mapping_a, prior_strength=1, top=1, ensemble=True)
-        MEMO_testing_step(memo, imageNet_A, mapping_a, test_name)
-
-        test_name = "DROP ImageNetV2, cut ensemble  without topk selection"
-        del memo, net
-        net = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-        net.layer4.add_module('dropout', nn.Dropout(0, inplace=True))
-        memo = EasyMemo(net.to(device), device, mapping_v2, prior_strength=1, top=1, ensemble=True)
-        MEMO_testing_step(memo, imageNet_V2, mapping_v2, test_name)
-
-        test_name = "DROP ImageNetA, cut ensemble  with topk selection"
-        del memo, imageNet_V2, imageNet_A, net
-        imageNet_A, imageNet_V2 = memo_get_datasets('cut', 64)
-        net = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-        net.layer4.add_module('dropout', nn.Dropout(0, inplace=True))
-        memo = EasyMemo(net.to(device), device, mapping_a, prior_strength=1, top=0.1, ensemble=True)
-        MEMO_testing_step(memo, imageNet_A, mapping_a, test_name)
-
-        test_name = "DROP ImageNetV2, cut ensemble  with topk selection"
-        del memo, net
-        net = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-        net.layer4.add_module('dropout', nn.Dropout(0, inplace=True))
-        memo = EasyMemo(net.to(device), device, mapping_v2, prior_strength=1, top=0.1, ensemble=True)
-        MEMO_testing_step(memo, imageNet_V2, mapping_v2, test_name)
+    for t in tests:
+        if tests[t]["run"]:
+            arg = base_test | tests[t]
+            arg['memo'] = base_test['memo'] | tests[t]['memo']
+            arg['dataset'] = base_test['dataset'] | tests[t]['dataset']
+            print(arg['memo']['device'])
+            MEMO_testing_step(t, arg)
