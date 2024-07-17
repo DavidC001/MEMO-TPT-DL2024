75747ba63fc18fd24cf8b2324e944675b631fadc
lorenzoorsi <lorenzo.orsingher@gmail.com>
2024-07-17 15:37:07 +0200

    add easyaugmenter documentation

--- dataloaders/easyAugmenter.py
+++ dataloaders/easyAugmenter.py
@@ -6,11 +6,25 @@ import numpy as np
 
 class EasyAgumenter(object):
     def __init__(self, base_transform, preprocess, augmentation, n_views=63):
+        """
+        This class provides an easy way to apply custom augmentations to images, the when called
+        it will return a list of augmentations with the original image in first place.
+
+        Args:
+
+        - base_transform (torchvision.transforms.Compose): The base transformation to apply to the images.
+        - preprocess (torchvision.transforms.Compose): The preprocessing transformation to apply to the images (will be applied last).
+        - augmentation (str): The type of augmentation to apply, can be 'augmix', 'identity' or 'cut'.
+        - n_views (int): The number of augmentations to apply to the image.
+
+        Returns:
+        - (list) A list of images with the augmentations applied.
+        """
         self.base_transform = base_transform
         self.preprocess = preprocess
         self.n_views = n_views
 
-        if augmentation == 'augmix':
+        if augmentation == "augmix":
 
             self.preaugment = transforms.Compose(
                 [
@@ -19,9 +33,9 @@ class EasyAgumenter(object):
                     transforms.CenterCrop(224),
                 ]
             )
-        elif augmentation == 'identity':
+        elif augmentation == "identity":
             self.preaugment = self.base_transform
-        elif augmentation == 'cut':
+        elif augmentation == "cut":
             self.preaugment = transforms.Compose(
                 [
                     transforms.RandomResizedCrop(224),
@@ -29,8 +43,8 @@ class EasyAgumenter(object):
                 ]
             )
         else:
-            raise ValueError('Augmentation type not recognized')
-    
+            raise ValueError("Augmentation type not recognized")
+
     def __call__(self, x):
 
         if isinstance(x, np.ndarray):
